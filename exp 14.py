44fcbe34030af3dd6ca369e16442190dce9a697d
192424332simats-dotcom <192424332.simats@saveetha.com>
2026-03-04 21:47:58 +0530

    Update exp 14.py

--- exp 14.py
+++ exp 14.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-# Sample sales data stored in a DataFrame
 data = {
     "Customer_ID": [101,102,103,104,105,106,107,108],
     "Age": [22,25,22,30,28,25,30,22],
@@ -9,12 +8,11 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Display DataFrame
 print("Sales Data:\n")
 print(df)
 
-# Frequency distribution of ages
 age_frequency = df["Age"].value_counts().sort_index()
 
 print("\nFrequency Distribution of Customer Ages:\n")
 print(age_frequency)
+
